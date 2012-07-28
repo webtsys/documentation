7a06542ac145762e7ea93671f0e599bc986fc276
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-07-29 01:15:54 +0200

    Added documentation block for blocks

---
+++ blocks/extra_links/documentation.php
@@ -0,0 +1,12 @@
+<?php
+
+global $lang;
+
+load_lang('documentation');
+
+$select_page[]=$lang['documentation']['documentation'];
+$select_page[]=make_fancy_url($base_url, 'documentation', 'index', 'documentation', $arr_data=array());
+
+
+
+?>
