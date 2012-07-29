5fce3d046c3d4b18bfb6e9ec324a38320711487b
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-07-29 03:55:00 +0200

    Little fix in links for blocks

--- blocks/extra_links/documentation.php
+++ blocks/extra_links/documentation.php
@@ -4,7 +4,13 @@ global $lang;
 
 load_lang('documentation');
 
+$select_page[]=$lang['translations']['translations'];
+$select_page[]='optgroup';
+
 $select_page[]=$lang['documentation']['documentation'];
 $select_page[]=make_fancy_url($base_url, 'documentation', 'index', 'documentation', $arr_data=array());
+
+$select_page[]='';
+$select_page[]='end_optgroup';
 
 ?>
