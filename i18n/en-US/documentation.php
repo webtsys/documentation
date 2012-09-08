64f35b9cfe5e461868f2beea0993918b2df667b1
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-09-08 03:52:34 +0200

    Fixes in translations en-US

--- i18n/en-US/documentation.php
+++ i18n/en-US/documentation.php
@@ -2,33 +2,33 @@
 
 $lang['documentation']['documentation']='Documentation';
 
-$lang['documentation']['end_book']='end_book';
+$lang['documentation']['end_book']='End book';
 
-$lang['documentation']['first_chapter_book']='first_chapter_book';
+$lang['documentation']['first_chapter_book']='First chapter of book';
 
-$lang['documentation']['no_exists_book']='no_exists_book';
+$lang['documentation']['no_exists_book']='The book don&#39;t exists';
 
-$lang['documentation']['show_menu']='show_menu';
+$lang['documentation']['show_menu']='Show menu';
 
-$lang['documentation']['hidden_menu']='hidden_menu';
+$lang['documentation']['hidden_menu']='Close menu';
 
-$lang['documentation']['error_cannot_access_to_ajax']='error_cannot_access_to_ajax';
+$lang['documentation']['error_cannot_access_to_ajax']='Error: cannot access to ajax';
 
-$lang['documentation']['books']='books';
+$lang['documentation']['books']='Books';
 
-$lang['documentation']['edit_books']='edit_books';
+$lang['documentation']['edit_books']='Edit books';
 
-$lang['documentation']['documentation_index']='documentation_index';
+$lang['documentation']['documentation_index']='Documentation index';
 
-$lang['documentation']['modify_order_chapters']='modify_order_chapters';
+$lang['documentation']['modify_order_chapters']='Modify chapter orders';
 
-$lang['documentation']['go_to_index_books']='go_to_index_books';
+$lang['documentation']['go_to_index_books']='Go to home books';
 
-$lang['documentation']['edit_chapters']='edit_chapters';
+$lang['documentation']['edit_chapters']='Edit chapters';
 
-$lang['documentation']['edit_son_docs']='edit_son_docs';
+$lang['documentation']['edit_son_docs']='Edit son of this chapter';
 
-$lang['documentation']['no_next_chapter']='no_next_chapter';
+$lang['documentation']['no_next_chapter']='No more chapters';
 
 $lang['documentation']['next_chapter']='next_chapter';
 
